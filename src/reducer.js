0767d50a58230f0209ae76bd204a6ee1b7de7062
Oleksii Ustinov <ustinovoleksij@gmail.com>
2021-12-27 19:45:13 +0200

    Added the function

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,17 @@
-function reducer(input, action) {
-  // Ваше решение тут
+const string = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam dolor velit quas soluta dicta sunt nesciunt repellendus quia error minus assumenda, autem animi esse rem suscipit ut quaerat deserunt inventore.';
+const action = 'Choose the action: capitalize, to upper case, to lower case'
+function reducer(string, action) {
+  var string = document.getElementById('string'),
+  if(action="capitalize") {
+    return string.toUpperCase;
+  } else if(action="to upper case"){
+    return string.toUpperCase;
+  } else if (action="to lower case"){
+    return string.toLowerCase;
+  } else if(action="null") {
+    return alert("You didn't choose the action")
+  };
+
 }
 
 module.exports = reducer;
